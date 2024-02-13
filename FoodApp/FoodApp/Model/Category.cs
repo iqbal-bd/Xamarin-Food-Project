f24ae77038334daae77aa5fcd6a60e43ce00bcf5
Md Iqbal Hossain <iqbalhossainnbd@gmail.com>
2024-02-13 22:55:07 +0600

    Revert "Relationship with tables and Added Dependencies"
    This reverts commit 6d3cec7bd710b6175b11d7caff5c2fe536801096.

---
+++ FoodApp/FoodApp/Model/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodApp.Model
+{
+    public class Category
+    {
+        public  int id { get; set; }
+        public string Title { get; set; }
+        public string ImageUri { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
